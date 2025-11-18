9800887b51de54c59a5a13943163380e2a4b8850
KhuswanthRaoJadav <khuswanthraojadav@gmail.com>
2025-11-18 10:51:07 +0530

    Add files via upload
    Replace old file

---
+++ src/pages/Checkout/Checkout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PaymentPage = () => {
+  return (
+    <div className="p-6 text-center text-lg">
+      Payment Page Loaded Successfully
+    </div>
+  );
+};
+
+export default PaymentPage;
